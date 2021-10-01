acdf1535362a40e612cf0b6e3c83fffc889ff398
Shivam Agrawal <shivam301296@gmail.com>
2021-10-01 09:56:51 +0530

    json input

--- app.py
+++ app.py
@@ -1,15 +1,27 @@
 from flask import Flask, json, request, jsonify, send_file
 from iengine import resize, paste_logo
 from PIL import Image
-from inputs import docs
-import os 
+import os
+import json
+import time
+
+
+# ========================================================
+output_dir = "./out"
+input_json_path = "./inputs.json"
+update_fq = 20  # seconds
+# ========================================================
+
 
 app = Flask(__name__)
 
+ts = 0
+idocs = {"docs": []}
 
-@app.route("/", methods=['GET'])
+
+@app.route("/", methods=["GET"])
 def index():
-    return jsonify({"message": "Hello World"})
+    return jsonify({"message": "Hello World From Image Resizer"})
 
 
 @app.route("/resize", methods=["POST"])
@@ -28,16 +40,27 @@ def resize_img():
 
 @app.route("/test", methods=["POST"])
 def test():
+    global ts
+    global idocs
     logo = request.files["logo"]
 
+    cts = time.time()
+    if cts - update_fq > ts:
+        ts = cts
+        print("======================Reading files=====================")
+
+        idocs_file = open(
+            "./inputs.json",
+        )
+        idocs = json.load(idocs_file)
+
     count = 0
-    for doc in docs:
-        name= os.path.splitext(doc["path"])[0]
+    for doc in idocs["docs"]:
+        name = os.path.splitext(doc["path"])[0]
         mwidth = doc["width"]
         mheight = doc["height"]
         print("Resizing logo==================")
-        resized_img = resize(logo, mwidth, mheight,
-                             doc["halign"], doc["valign"])
+        resized_img = resize(logo, mwidth, mheight, doc["halign"], doc["valign"])
         resized_img.save("out/for_{}.png".format(name))
         print("Putting Logo on Doc============")
         paste_logo(doc, resized_img, count)
@@ -47,7 +70,57 @@ def test():
     return jsonify({"message": "success"})
 
 
-if(__name__ == "__main__"):
+@app.route("/resize_all", methods=["POST"])
+def resize_all():
+    global ts
+    global idocs
+    logo = request.files["logo"]
+    logoname = request.form.get("folder_name")
+
+    if not logoname:
+        logoname = os.path.splitext(logo.filename)[0]
+
+    msg = ""
+
+    cts = time.time()
+    if cts - update_fq > ts:
+        print("======================Reading files=====================")
+        try:
+            idocs_file = open(
+                input_json_path,
+            )
+            idocs = json.load(idocs_file)
+            ts = cts
+        except OSError:
+            msg = "JSON file not found"
+        except ValueError:
+            msg = "Incorrect json file format"
+        if msg == "":
+            msg = "Total docs updated: {}".format(len(idocs["docs"]))
+
+    out_folder = os.path.join(output_dir, logoname)
+    try:
+        os.makedirs(out_folder)
+    except OSError as error:
+        print(error)
+
+    out_paths = {}
+    for doc in idocs["docs"]:
+        name = os.path.splitext(doc["path"])[0]
+        resized_img = resize(
+            logo, doc["width"], doc["height"], doc["halign"], doc["valign"]
+        )
+        out_path = os.path.join(out_folder, "Resized__{}.png".format(name))
+        resized_img.save(out_path)
+        out_paths[doc["path"]] = out_path
+
+    return jsonify({"status": "success", "message": msg, "paths": out_paths})
+
+
+if __name__ == "__main__":
     app.run(host="0.0.0.0", port=2001)
 # https://www.image-map.net/
 # 18001200
+
+
+9425604658
--- iengine.py
+++ iengine.py
@@ -2,26 +2,26 @@ from PIL import Image
 
 
 def get_max_size(iw, ih, mw, mh):
-    rw = mw/iw
-    rh = mh/ih
+    rw = mw / iw
+    rh = mh / ih
 
     final_ratio = rw if rw < rh else rh
-    return int(final_ratio*iw), int(final_ratio*ih)
+    return int(final_ratio * iw), int(final_ratio * ih)
 
 
 def get_top_left(iw, ih, mw, mh, halign, valign):
     top = 0
     left = 0
 
-    if(halign == "right"):
+    if halign == "right":
         left = mw - iw
-    elif(halign == "center"):
-        left = int((mw-iw)/2)
+    elif halign == "center":
+        left = int((mw - iw) / 2)
 
-    if(valign == "bottom"):
-        top = mh-ih
-    elif(valign == "center"):
-        top = int((mh-ih)/2)
+    if valign == "bottom":
+        top = mh - ih
+    elif valign == "center":
+        top = int((mh - ih) / 2)
 
     return top, left
 
@@ -40,16 +40,18 @@ def resize(imgfile, mwidth, mheight, halign="center", valign="center"):
     resized_img = img.resize((size[0], size[1]), Image.ANTIALIAS)
 
     top, left = get_top_left(
-        resized_img.size[0], resized_img.size[1], mwidth, mheight, halign, valign)
+        resized_img.size[0], resized_img.size[1], mwidth, mheight, halign, valign
+    )
     print("Top, Left: {},{}".format(top, left))
 
-    out_img = Image.new(resized_img.mode, (mwidth, mheight), (255,))
+    # out_img = Image.new(resized_img.mode, (mwidth, mheight), (255,))
+    out_img = Image.new("RGBA", (mwidth, mheight), (255, 255, 255, 0))
     out_img.paste(resized_img, (left, top))
     return out_img
 
 
 def paste_logo(doc, logo, count):
-    out = Image.open("docs/{}".format(doc["path"])).convert('RGBA')
+    out = Image.open("docs/{}".format(doc["path"])).convert("RGBA")
     temp = Image.new("RGBA", out.size, (255,))
     temp.paste(logo, (doc["left"], doc["top"]))
     out.alpha_composite(temp)
